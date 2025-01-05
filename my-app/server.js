b28e5ab3ade7ef906bb5ff1bd6153831b7f0bcdd
reuel.rai <149403520+reuelrai@users.noreply.github.com>
2025-01-05 19:23:29 +0545

    Update server.js

--- my-app/server.js
+++ my-app/server.js
@@ -69,3 +69,12 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
 });
+
+const express = require('express');
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
